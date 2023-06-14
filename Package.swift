6340dd7ffd31ae880a3daf976cb89c3db2722317
GFWFighter <GFWFighter@users.noreply.github.com>
2023-06-14 18:27:32 +0000

    XRay Release 1.0.0!!!

--- Package.swift
+++ Package.swift
@@ -2,16 +2,16 @@
 import PackageDescription
 
 let package = Package(
-  name: "XrayKit",
-  platforms: [.iOS(.v12), .macOS(.v12)],
+  name: "XRay",
+  platforms: [.iOS(.v15)],
   products: [
-    .library(name: "XrayKit", targets: ["XrayKit"])
+    .library(name: "XRay", targets: ["XRay"])
   ],
   targets: [
     .binaryTarget(
-      name: "XrayKit",
-      url: "https://github.com/arror/XrayKit/releases/download/0.1.79/XrayKit.xcframework.zip",
-      checksum: "c435fa9e433affcd399c1bbe43908d5fb7741d748f4b93a3ca7d3edfd0ea75f0"
+      name: "XRay",
+      url: "https://github.com/GFWFighter/XRayApple/releases/download/1.0.0/XRay.xcframework.zip",
+      checksum: "c3ead1b85303fab05009511f0a7a6a42b6ce38ea1345938f98d594c1eee3eef3"
     )
   ]
 )
