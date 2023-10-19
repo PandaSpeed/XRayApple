9cc14cb848177aa76008eb40d339fc3a1bcd5fa2
GFWFighter <GFWFighter@users.noreply.github.com>
2023-10-19 18:44:26 +0000

    XRay Release 1.8.4!!!

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "XRay",
-      url: "https://github.com/GFWFighter/XRayApple/releases/download/1.1.1/XRay.xcframework.zip",
-      checksum: "a783efc42508ad78f75f99105e8b5c6435b67ea4ba320197ebd36cd8d60bcca4"
+      url: "https://github.com/GFWFighter/XRayApple/releases/download/1.8.4/XRay.xcframework.zip",
+      checksum: "ea58566c97e1a41529dded1aa8c39a8b8756d129b5bfec734061a646db279227"
     )
   ]
 )
